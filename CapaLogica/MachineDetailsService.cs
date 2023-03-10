4e30e3accdfbb3ce50b9e3d8e206982f12984a50
Luis Developer <luismq7890@gmail.com>
2023-03-10 00:08:18 -0500

    Se implementa la clase WebSocketInitializer para conectar al web socket del servicio Backend

---
+++ CapaLogica/MachineDetailsService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Net;
+
+namespace CapaLogica
+{
+    public  class MachineDetailsService
+    {
+        string CurrentMachineMacAddres = "";
+        string CurrentMachineIp = "";
+        NetworkInterface NetInfo;
+        PhysicalAddress ObjMacAddress;
+        IPAddress ObjIpAddress;
+
+        public MachineDetailsService()
+        {
+            NetInfo = NetworkInterface.GetAllNetworkInterfaces()
+            .FirstOrDefault(n => n.OperationalStatus == OperationalStatus.Up 
+            && n.NetworkInterfaceType != NetworkInterfaceType.Loopback);
+        }
+
+
+
+        public string GetMacAddressMachine()
+        {
+            ObjMacAddress = NetInfo.GetPhysicalAddress();
+            CurrentMachineMacAddres = BitConverter.ToString(ObjMacAddress.GetAddressBytes());
+            return CurrentMachineMacAddres;
+
+        }
+
+        public string GetIpAddressMachine()
+        {
+            ObjIpAddress = NetInfo.GetIPProperties()
+                .UnicastAddresses
+                .FirstOrDefault(Ip => Ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)?.Address;
+
+            CurrentMachineIp = ObjIpAddress?.ToString() ?? "null";
+            return CurrentMachineIp;
+
+        }
+
+
+
+    }
+}
---
+++ CapaLogica/WebSocketServiceIntializer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using CapaModelo.Common;
+
+namespace CapaLogica
+{
+    public class WebSocketServiceIntializer
+    {
+        MachineDetailsService MachineDetails;
+        WebSocket LanCenterWebSocket;
+        string WebSocketUrl = "";
+
+        public WebSocketServiceIntializer()
+        {
+            Init();
+            
+        }
+
+       void Init()
+        {
+            MachineDetails = new MachineDetailsService();
+            LanCenterWebSocket = new WebSocket(WebSocketUrl);
+        }
+
+
+        public void InitLanWebSocket()
+        {
+            LanCenterWebSocket.Connect();
+            
+        }
+
+        public void SendMessageToServer(string Message)
+        {
+            LanCenterWebSocket.Send(Message);
+        }
+
+        public void SendMessageToServer(byte[] MessageBytes)
+        {
+            LanCenterWebSocket.Send(MessageBytes);
+        }
+    }
+}
